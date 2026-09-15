161de40fb3447bd553d90d9df3a169f3cb5047e8
jarndtphoto <jarndtphoto@gmail.com>
2026-09-15 18:47:57 -0500

    Load refreshed app layout styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./bright-theme.css";
+import "./layout-refresh.css";
 import "leaflet/dist/leaflet.css";
 import AppUpdateChecker from "./components/AppUpdateChecker";
 
