85d3ebf00ced75bde6fb000a8ce05289fc6dda38
jarndtphoto <jarndtphoto@gmail.com>
2026-09-15 14:27:52 -0500

    Fix Home Screen manifest icon purpose

--- app/manifest.ts
+++ app/manifest.ts
@@ -16,13 +16,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/api/app-icon?size=192",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/api/app-icon?size=512",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
     ],
   };
